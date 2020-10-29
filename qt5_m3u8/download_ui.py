16b8910bb2a38535c70174241cb1e3608c3ee50f
1di0t <dgqcjx@gmail.com>
2020-10-29 18:29:05 +0800

    fix

--- qt5_m3u8/download_ui.py
+++ qt5_m3u8/download_ui.py
@@ -14,6 +14,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
+        MainWindow.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.WindowCloseButtonHint)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.download_link_label = QtWidgets.QLabel(self.centralwidget)
