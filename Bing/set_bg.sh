dda9f62cbd6077213e4766f5c27d6fc2d4a32418
1di0t <daiguangquan@sf-pharma.com>
2022-10-12 15:58:15 +0800

    优化

--- Bing/set_bg.sh
+++ Bing/set_bg.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-BASE_DIR='/home/1di0t/Pictures/Bing/'
+BASE_DIR="${HOME}/Pictures/Bing/"
 
-random_pic_name=`ls $BASE_DIR | shuf -n1`
-
-random_pic_path=$BASE_DIR$random_pic_name
-
-set_background(){
-	random_pic_name=`ls $BASE_DIR | shuf -n1`
-	random_pic_path=$BASE_DIR$random_pic_name
-	/usr/bin/gsettings set org.gnome.desktop.background picture-uri file://$random_pic_path
+set_background() {
+  random_pic_name=`ls $BASE_DIR | shuf -n1`
+  random_pic_path=$BASE_DIR$random_pic_name
+  echo "change background: ""$random_pic_name"
+  picture_uri="file://${random_pic_path}"
+  /usr/bin/gsettings set org.gnome.desktop.background picture-uri-dark "${picture_uri}"
+  /usr/bin/gsettings set org.gnome.desktop.background picture-uri "${picture_uri}"
 }
 
 set_background
-
